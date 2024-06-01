95b917fc2a00abc5f5f79e15716bf76dc9f5af1a
lifei6 <lifei6@users.noreply.github.com>
2024-06-01 13:26:47 +0000

    deploy: 842eec0a7ebd3d1e7ffbdc98c5f085e86e676d4a

---
+++ assets/app.DmPrOkl2.js
@@ -0,0 +1,7 @@
+import{L as s,ag as p,ah as u,ai as l,aj as c,ak as f,al as d,am as m,an as h,ao as A,ap as g,aa as v,d as P,H as R,M as w,R as y,aq as C,ar as _,as as E,h as b}from"./chunks/framework.DRSb6gpS.js";import{R as D}from"./chunks/theme.IQFKgHhp.js";function i(e){if(e.extends){const a=i(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const o=i(D),L=P({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=R();return w(()=>{y(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&C(),_(),E(),o.setup&&o.setup(),()=>b(o.Layout)}});async function T(){const e=O(),a=j();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),o.enhanceApp&&await o.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function j(){return h(L)}function O(){let e=s,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),s&&(e=!1),r},o.NotFound)}s&&T().then(({app:e,router:a,data:t})=>{a.go().then(()=>{p(a.route,t.site),e.mount("#app")})});export{T as createApp};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- assets/introduction.md.CzrAt6Pw.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,o as t,a2 as i}from"./chunks/framework.BmV95l8e.js";const b=JSON.parse('{"title":"介绍","description":"","frontmatter":{},"headers":[],"relativePath":"introduction.md","filePath":"introduction.md"}'),l={name:"introduction.md"},r=i("",15),o=[r];function n(s,h,u,d,c,_){return t(),e("div",null,o)}const f=a(l,[["render",n]]);export{b as __pageData,f as default};
